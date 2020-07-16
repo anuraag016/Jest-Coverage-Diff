8f41b74a9d4ddd1efe0af7e2b8c2c918c1794424
Anuraag Puri <anuraag016@github.com>
2020-07-16 17:35:55 +0530

    Initial test code

---
+++ src/Model/CoveragePercentage.ts
@@ -0,0 +1,8 @@
+export interface CoveragePercentages {
+  fileName: string
+  statements: number
+  branch: number
+  func: number
+  lines: number
+  uncoveredLines: string
+}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,46 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {execSync} from 'child_process'
+import {CoverageParser} from './CoverageParser'
+import {CoverageDiffChecker} from './CoverageDiffChecker'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const repoName = github.context.repo.repo
+    const repoOwner = github.context.repo.owner
+    const githubToken = core.getInput('accessToken')
+    const fullCoverage = core.getInput('fullCoverageDiff')
+    // const optionalArgs = core.getInput('optionalArgs');
+    const commandToRun = 'npx jest --coverage'
+    // const options: core.InputOptions = { required: true };
+    const githubClient = github.getOctokit(githubToken)
+    const prNumber = github.context.issue.number
+    const branchNameBase = github.context.payload.pull_request?.base.ref
+    const branchNameHead = github.context.payload.pull_request?.head.ref
+    const codeCoverageNew = execSync(commandToRun).toString()
+    execSync('/usr/bin/git fetch')
+    execSync(`/usr/bin/git checkout --progress --force ${branchNameBase}`)
+    const codeCoveragePrev = execSync(commandToRun).toString()
+    const coverageParserNew = new CoverageParser(codeCoverageNew)
+    const coverageParserPrev = new CoverageParser(codeCoveragePrev)
+    const coverageDiffChecker = new CoverageDiffChecker(
+      coverageParserPrev,
+      coverageParserNew
+    )
+    let postMessage = `${coverageParserNew.headerLines[1]}\n${coverageParserNew.headerLines[2]}\n`
+    if (fullCoverage) {
+      postMessage += coverageDiffChecker.getFullCoverageWithDiff().join('\n')
+    } else {
+      postMessage += coverageDiffChecker.getOnlyDiffLines().join('\n')
+    }
+    await githubClient.issues.createComment({
+      repo: repoName,
+      owner: repoOwner,
+      body: `Code coverage comparison ${branchNameBase} vs ${branchNameHead}: \n ${postMessage}`,
+      issue_number: prNumber
+    })
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error)
   }
 }
 
